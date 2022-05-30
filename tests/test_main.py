0ffd2dd80b323d4f9563ff7eee5013d3c61366d8
Anton Horodchuk <antoman1994@gmail.com>
2022-05-30 16:38:53 +0300

    xfail for tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from pytest import fixture
+from pytest import fixture, mark
 
 from fastapi.testclient import TestClient
 from fastapi_background_tasks_test.main import app
@@ -30,11 +30,13 @@ class TestMain:
     def clock(self):
         return Clock()
 
+    @mark.xfail
     def test_sync(self, test_client, clock):
         with clock as cl:
             test_client.get('/sync')
             assert cl.elapsed() < 1
 
+    @mark.xfail
     def test_async(self, test_client, clock):
         with clock as cl:
             test_client.get('/async')
@@ -45,6 +47,7 @@ class TestMain:
             test_client.get('/async_sync')
             assert cl.elapsed() < 1
 
+    @mark.xfail
     def test_async_with_async_lock(self, test_client, clock):
         with clock as cl:
             test_client.get('/async_with_async_lock')
